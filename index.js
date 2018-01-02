f40de5bca28304fb71f8d8025f25406abc9de120
edarce09 <edarce09@gmail.com>
2018-01-01 19:59:59 -0500

    gone message

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const ok = function succesfullOperation(res, data) {
   return res.status(200).json(response)
 }
 
+const created = function documentSuccessfullyCreated(res, data){
+  let response = {
+    code:'created',
+    message: 'The new resource has been created successfully',
+    data: data||{}
+  };
+  return res.status(201).json(response);
+}
+
 const notFound = function informationNotFounded(res, data) {
   let response = {
     code: 'ERROR_NOT_FOUND',
@@ -16,7 +25,17 @@ const notFound = function informationNotFounded(res, data) {
   return res.status(404).json(response);
 }
 
-const serverError = function serverError(res, data, err){
+const gone = function permanentlyRemoved(res, data){
+  let response = {
+    code: 'GONE',
+    message: 'The access to the target is no longer avaible',
+    data: data || {} 
+    };
+  return res.status(410).json(response);
+}
+
+
+const serverError = function (res, data, err){
  let responce = {
    code: 'ERROR_INTERNAL_SERVER_ERROR',
    message: err || 'An error has ocurred with in the server', 
@@ -24,6 +43,7 @@ const serverError = function serverError(res, data, err){
  };
  return res.status(500).json(response);
 }
+
 
 module.exports = {
   ok,
